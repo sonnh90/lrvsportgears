4ffda15c47cb0d509aedd2db8e2227c93f7a648e
sonnh90 <sonnh2109@gmail.com>
2024-08-28 09:55:43 +0700

    Add debug route
    Add debug route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/** Debug */
+Route::get('/debug/dbinfo' , function(){
+    dd( env( 'DB_DATABASE' ) );
+});
